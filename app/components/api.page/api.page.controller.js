c8093d12f8af806d09460c9f3b037cbbb1925095
Mike Dowd <mikedowd1972@gmail.com>
2017-03-21 11:17:54 -0600

    added more examples in the API docs

--- app/components/api.page/api.page.controller.js
+++ app/components/api.page/api.page.controller.js
@@ -4,6 +4,9 @@ angular.module('app')
       const vm = this;
 
       vm.$onInit = function() {
+        vm.htmlStr1 = '<audio id="sound-${soundArray[i].id || soundArray[i].sound_id}">';
+        vm.htmlStr2 = '    <source src="${soundArray[i].url}"  type="audio/mpeg">';
+        vm.htmlStr3 = '</audio>';
       }
 
     },
